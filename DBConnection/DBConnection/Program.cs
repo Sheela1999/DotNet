29347fd60d6092ba024e171d37d6ae849ba516c9
Sheela1999 <sheela.xworkz@gmail.com>
2024-04-02 16:34:10 +0530

    DB Conn

---
+++ DBConnection/DBConnection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+namespace DBConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn =  new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=dotNet;Integrated Security=True;");
+            SqlCommand cmd = new SqlCommand("Select * from Employee",conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine(reader[0] + "\t" + reader[1] + "\t" + reader[2] + "\t" + reader[3] + "\t" + reader[4]);
+            }
+
+        }
+    }
+}
